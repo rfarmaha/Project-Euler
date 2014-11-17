5975b594f2b9ca3a08ce054d92a6d504164931c9
Ramandeep Farmaha <ramandeep.f@gmail.com>
2014-11-17 12:39:50 -0500

    Create solution.cpp
    OUTPUT
    LCM: 232792560

---
+++ Problem-5/solution.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+long computeGCD (long a, long b);
+long computeLCM (long a, long b);
+int main()
+{
+   //Declare variables
+   long lcm = 20;
+
+   for (long i = lcm; i > 1; i--)
+    {
+        lcm = computeLCM(lcm, i-1);
+    }
+    cout << "LCM: " << lcm;
+   return 0;
+}
+
+long computeGCD (long a, long b)
+{
+    //Declare variables used in function
+    long c = 0;
+    long d = 0;
+    long e = 1; 
+    
+    //Allow for arbitrary place of larger value
+    if (a > b)
+    {
+        c = a;
+        d = b;
+    }
+    else if (b > a)
+    {
+        c = b;
+        d = a;
+    }
+    
+    //Case where both numbers are equivalent
+    else
+    {
+        return a;
+    }
+    
+    //Using Euclidian Algorithm
+    while (e != 0)
+    {
+        e = c % d;
+        c = d;
+        d = e; 
+    }
+    
+    return c; 
+}
+
+long computeLCM (long a, long b)
+{
+    return (a * b)/ (computeGCD(a, b));
+}
+
+
+
